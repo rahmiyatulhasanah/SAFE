ad5ffd9a4305e6928ef8783d09f78841678aca83
rahmiyatulhasanah <74273223+rahmiyatulhasanah@users.noreply.github.com>
2021-04-22 20:20:46 +0700

    Add files via upload

---
+++ test/test/Pengguna.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SAFE_project
+{
+    class Pengguna
+    {
+        private string email;
+        private string password;
+        private string namaPengguna;
+        private int noTelp;
+        private string GolDar;
+        //private string alergiObat;
+        //private string riwayatPenyakit;
+
+        public void input ()
+        {
+            Pengguna dataPengguna = new Pengguna;
+            //code here
+        }
+
+        public void cari()
+        {
+            //code here
+        }
+
+        public void pilih()
+        {
+            //code here
+        }
+
+        public void telp()
+        {
+            //code here
+        }
+    }
+}
