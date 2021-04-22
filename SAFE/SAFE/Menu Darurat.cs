c9f5f74ea3bd218e0dacd5a6e0ce44145a9ee33a
rahmiyatulhasanah <74273223+rahmiyatulhasanah@users.noreply.github.com>
2021-04-22 20:26:03 +0700

    Add files via upload

---
+++ SAFE/SAFE/Menu Darurat.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SAFE_project
+{
+    class Menu_Darurat
+    {
+        public int noTelpInstansi;
+        public string keadaanDarurat;
+        public string solusiPenanganan;
+
+        public void tampil()
+        {
+            //code here
+        }
+    }
+}
