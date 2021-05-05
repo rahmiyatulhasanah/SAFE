652123002064e9b7c9865edb01658e2566a7d137
Putri Ayu Shintaningrum <79202858+putriashn@users.noreply.github.com>
2021-05-06 01:57:27 +0700

    Add files via upload

---
+++ SAFE 2/SAFE/MenuDarurat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAFE
+{
+    public class MenuDarurat
+    {
+        public int noTelpInstansi;
+        public string keadaanDarurat;
+        public string solusiPenanganan;
+        public string infoKeadaan;  
+    }
+
+    class KeadaanDarurat : MenuDarurat
+    {
+        public void tampilkan_info(string infoKeadaan)
+        {
+            Console.WriteLine(infoKeadaan);
+        }
+
+        public void tampilkan_penanganan (string solusiPenanganan)
+        {
+            Console.WriteLine(solusiPenanganan);
+        }
+    }
+}
